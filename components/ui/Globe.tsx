ef421800f996e42ac42d6c27d28afac81abdc449
WaleedDev-39 <m.waleed.a.03@gmail.com>
2025-09-30 00:07:39 +0500

    deploly error fix

--- components/ui/Globe.tsx
+++ components/ui/Globe.tsx
@@ -314,7 +314,7 @@ export function hexToRgb(hex: string) {
 }
 
 export function genRandomNumbers(min: number, max: number, count: number) {
-  const arr = [];
+  const arr: number[] = []; // <-- explicitly typed as number[]
   while (arr.length < count) {
     const r = Math.floor(Math.random() * (max - min)) + min;
     if (arr.indexOf(r) === -1) arr.push(r);
