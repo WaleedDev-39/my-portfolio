22c000b0ceadd204625ec5f339bfcfe140b52e41
WaleedDev-39 <m.waleed.a.03@gmail.com>
2025-09-29 22:39:46 +0500

    new changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import { Grid } from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -8,7 +10,7 @@ import { navItems } from "@/data";
 
 export default function Home() {
   return (
-    <main className="relative bg-black flex flex-col justify-center items-center overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black flex flex-col justify-center items-center mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems=
           {navItems}
@@ -19,6 +21,8 @@ export default function Home() {
         <RecentProjects />
         <Clients />
         <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
